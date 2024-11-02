777bd6ed7d01993a9b5d882504b15c0581cbbc72
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-11-02 20:36:54 +0530

    if

---
+++ ifgreatest.py
@@ -0,0 +1,33 @@
+# no1 = int(input("First No" ))
+
+# no2 = int(input("Second No" ))
+
+# no3 = int(input("Third No" ))
+
+# if no2>no1 and no2>no3 :
+#     print(str(no2) + "largest no")
+
+# elif no3>no1 and no3> no2 :
+#     print  (str(no3) + "largest no")
+
+# else :
+#     print (str(no1) + "numbe is the largest")
+
+
+no1 = int(input("First No" ))
+
+no2 = int(input("Second No" ))
+
+no3 = int(input("Third No" ))
+
+if (no2>no1 and no2>no3) :
+    print( "largest no is", no2 )
+
+elif (no3>no1 and no3> no2) :
+    print  ( "largest no is", no3 )
+
+else :
+    print ( " largest no is ", no1)
+
+
+
---
+++ ifodd.py
@@ -0,0 +1,6 @@
+number = int(input('Write a number to check: '))
+
+if number % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
