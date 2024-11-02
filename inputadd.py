75e5e0dd119613bd445f440662a0aef29b8d4ba0
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-11-02 13:54:56 +0530

    input.py

---
+++ inputadd.py
@@ -0,0 +1,8 @@
+a = int(input('First No Is' ))  #here I want to input no
+
+b  = int (input('Scond No is' ))
+
+c = a+b
+
+print(c)
+
---
+++ inputboolen.py
@@ -0,0 +1,5 @@
+a = int(input("Fist No:"))
+
+b= int(input('second No:'))
+
+print(a> b)
