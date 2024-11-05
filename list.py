7e760f1359ecf37d3dace869d5122775194d4331
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-11-05 11:20:25 +0530

    dictionary

---
+++ list.py
@@ -0,0 +1,11 @@
+movie1 = input("write a movie")
+movie2 = input("write a another movie")
+movie3 = input("write a  another movie")
+
+movies = [movie1, movie2, movie3]
+
+print(movies)
+
+
+
+
---
+++ palindeome.py
@@ -0,0 +1,12 @@
+#palindrome---It is a list where all side show same
+
+#check the list is plindrome
+
+nos = [ "madam"]
+
+
+if nos == nos.reverse() : 
+
+    print ("it is Palidrome")
+else :
+    print ("not palindrome")
